483f85599cd64101bafb722d456bd61bb397f308
K. Kanchev <no thank you>
2013-03-30 14:15:21 +0200

    Exam C# I BitBall
    Закоментиран е излишния изход и е обърнат последният ред за да дава 100
    точки в bgcoder

--- C#/C# Part 01/ExamCSharp01/BitBall/BitBall.cs
+++ C#/C# Part 01/ExamCSharp01/BitBall/BitBall.cs
@@ -14,14 +14,14 @@ class BitBall
 		int topScore = 0;
 		int bottomScore = 0;
 		string text = "";
-		//for (int i = 0; i < 8; i++)
-		//{
-		//	topPlayers[i] = int.Parse(Console.ReadLine());
-		//}
-		//for (int i = 0; i < 8; i++)
-		//{
-		//	bottomPlayers[i] = int.Parse(Console.ReadLine());
-		//}
+		for (int i = 0; i < 8; i++)
+		{
+			topPlayers[i] = int.Parse(Console.ReadLine());
+		}
+		for (int i = 0; i < 8; i++)
+		{
+			bottomPlayers[i] = int.Parse(Console.ReadLine());
+		}
 
 		//solution
 		for (int i = 0; i < 8; i++)
@@ -36,20 +36,20 @@ class BitBall
 		for (int i = 0; i < 7; i++)
 		{
 			topPlayers[i + 1] = topPlayers[i + 1] | (~bottomPlayers[i + 1] & topPlayers[i]);
-			Console.WriteLine(Convert.ToString(topPlayers[i + 1], 2).PadLeft(8, '0'));
+			//Console.WriteLine(Convert.ToString(topPlayers[i + 1], 2).PadLeft(8, '0'));
 		}
-		Console.WriteLine();
-		Console.WriteLine(Convert.ToString(topPlayers[7], 2).PadLeft(8, '0'));
-		Console.WriteLine();
+		//Console.WriteLine();
+		//Console.WriteLine(Convert.ToString(topPlayers[7], 2).PadLeft(8, '0'));
+		//Console.WriteLine();
 		topPlayers = playersBuffer;
 		for (int i = 7; i > 0; i--)
 		{
 			bottomPlayers[i - 1] = bottomPlayers[i - 1] | (~topPlayers[i - 1] & bottomPlayers[i]);
-			Console.WriteLine(Convert.ToString(bottomPlayers[i - 1], 2).PadLeft(8, '0'));
+			//Console.WriteLine(Convert.ToString(bottomPlayers[i - 1], 2).PadLeft(8, '0'));
 		}
-		Console.WriteLine();
-		Console.WriteLine(Convert.ToString(bottomPlayers[0], 2).PadLeft(8, '0'));
-		Console.WriteLine();
+		//Console.WriteLine();
+		//Console.WriteLine(Convert.ToString(bottomPlayers[0], 2).PadLeft(8, '0'));
+		//Console.WriteLine();
 		text = String.Format("{0}", Convert.ToString(topPlayers[7], 2).PadLeft(8, '0'));
 		foreach (char count in text)
 		{
@@ -67,6 +67,6 @@ class BitBall
 			}
 		}
 		//print result
-		Console.WriteLine("{0}:{1}",topScore,bottomScore);
+		Console.WriteLine("{1}:{0}",topScore,bottomScore);
 	}
 }
